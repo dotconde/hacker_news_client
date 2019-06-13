5f9f724c74d5c95920325a27dceffd387d8692bc
byteconde <iamdeyvi@gmail.com>
2019-06-12 22:27:28 -0500

    Search input and button updated

--- src/App.js
+++ src/App.js
@@ -3,22 +3,39 @@ import {Component, useState, useEffect} from 'react';
 
 const App = () => {
   // State
-  const [news, setNews] = useState([])
+  const [news, setNews] = useState([]);
+  const [searchQuery,setSearchQuery] = useState('react');
+  const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react');
   // Fetch News from Hacker News API
   const fetchNews = () => {
-    fetch('http://hn.algolia.com/api/v1/search?query=react')
+    fetch(url)
     .then(result => result.json())
     .then(data => setNews(data.hits))
     .catch(error => console.log(error));
   };
 
   useEffect(()=>{
-    fetchNews()
-  })
+    fetchNews();
+  }, [url])
+
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+  }
 
   return(
     <div>
-      <b> Trending News About React </b>
+      <b> News </b>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={searchQuery} onChange={handleChange}/>
+        <button> Search </button>
+      </form>
+
       {news.map((n,i) => (
         <div>
         <p key={i}>{i+1}) {n.title}</p>
