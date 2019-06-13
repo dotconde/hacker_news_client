584f69ade522937c6660f7a11328c3b5c97b7ed0
byteconde <iamdeyvi@gmail.com>
2019-06-12 23:20:21 -0500

    Loading message

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ const App = () => {
   const [news, setNews] = useState([]);
   const [searchQuery,setSearchQuery] = useState('react');
   const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react');
-  
+  const [loading,setLoading] = useState(false)
   // Fetch News from Hacker News API
   const fetchNews = () => {
+    setLoading(true)
     fetch(url)
     .then(result => result.json())
-    .then(data => setNews(data.hits))
+    .then(data => (setNews(data.hits),setLoading(false)))
     .catch(error => console.log(error));
   };
 
@@ -31,7 +32,7 @@ const App = () => {
   return(
     <div>
       <b> News </b>
-
+      { loading ? <h2>Loading...</h2> : ""}
       <form onSubmit={handleSubmit}>
         <input type="text" value={searchQuery} onChange={handleChange}/>
         <button> Search </button>
