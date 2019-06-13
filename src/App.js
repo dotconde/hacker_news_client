d7b568f1e0308f2188696d0860e20cdbdfb6c077
byteconde <iamdeyvi@gmail.com>
2019-06-12 23:46:10 -0500

    Functional and divided code

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const App = () => {
   const [loading,setLoading] = useState(false)
   // Fetch News from Hacker News API
   const fetchNews = () => {
+    // Set loading true when fetching data from API
     setLoading(true)
     fetch(url)
     .then(result => result.json())
@@ -28,21 +29,32 @@ const App = () => {
     e.preventDefault();
     setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
   }
+  
+  const showLoading = () => (
+    loading ? <h2>Loading...</h2> : ""
+  )
+
+  const searchForm = () => (
+    <form onSubmit={handleSubmit}>
+        <input type="text" value={searchQuery} onChange={handleChange}/>
+        <button> Search </button>
+    </form>
+  )
+
+  const showNews = () => (
+    news.map((n,i) => (
+      <div>
+      <p key={i}>{i+1}) {n.title}</p>
+      </div>
+    ))
+  )
 
   return(
     <div>
       <b> News </b>
-      { loading ? <h2>Loading...</h2> : ""}
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={searchQuery} onChange={handleChange}/>
-        <button> Search </button>
-      </form>
-
-      {news.map((n,i) => (
-        <div>
-        <p key={i}>{i+1}) {n.title}</p>
-        </div>
-      ))}
+      { showLoading() }
+      { searchForm() }
+      { showNews() }
     </div>
   );
 };
